3be6e2e5dd93ea152dfbe665e4e52e034fe845fd
Chenxin Liu <chenxin.liu@etu.utc.fr>
2021-12-05 16:22:50 +0000

    Add new file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "splendor.h"
+using namespace Splendor;
+
+int main() {
+	try {
+		
+		
+	}
+	catch (SetException& e) {
+		std::cout << e.getInfo() << "\n";
+	}
+
+	system("pause");
+	return 0;
+}
