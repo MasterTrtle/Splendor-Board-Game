137a7079bec3fa482abfa90bc5ce187f2dda935f
Nicolas Vengeon <nicolas.vengeon@etu.utc.fr>
2021-12-11 15:58:02 +0000

    Replace splendor.cpp

--- splendor.cpp
+++ splendor.cpp
@@ -6,14 +6,19 @@ namespace Splendor {
 
 
 
-	
-	
-}
 
-std::ostream& operator<<(std::ostream& f, Cout c) {
-	f << "Vert:" << c.vert << "Bleu:" << c.bleu << "Noir:" << c.noir << "Rouge:" << c.rouge << "Blanc:" << c.blanc;
-}
-std::ostream& operator<<(std::ostream& f, const Carte& c) {
-	f << "(" << "ID:" << c.getID() << "," << "Nom:" << c.getNom() << "," << "Cout:" << c.getCout() << "," << "ID:Perstige" << c.getPrestige() << ")";
-	return f;
+	std::ostream& operator<<(std::ostream& f, Cout c) {
+		f << "Vert:" << c.vert << "Bleu:" << c.bleu << "Noir:" << c.noir << "Rouge:" << c.rouge << "Blanc:" << c.blanc;
+	}
+	std::ostream& operator<<(std::ostream& f, const Carte& c) {
+		f << "(" << "ID:" << c.getID() << "," << "Nom:" << c.getNom() << "," << "Cout:" << c.getCout() << "," << "ID:Perstige" << c.getPrestige() << ")";
+		return f;
+	}
+
+	Controleur::Controleur() {
+		piocheN1 = &Pioche(TypeCarte::N1);
+		piocheN2 = &Pioche(TypeCarte::N2);
+		piocheN3 = &Pioche(TypeCarte::N3);
+	};
+
 }
