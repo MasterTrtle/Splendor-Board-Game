8540191f8e2e9c5b962b88fb40b5c55f6a79b9df
Chenxin Liu <chenxin.liu@etu.utc.fr>
2021-12-12 11:37:31 +0000

    Update for methods jourer for splendor.cpp

--- splendor.cpp
+++ splendor.cpp
@@ -1,6 +1,25 @@
 #include "splendor.h"
 
 namespace Splendor {
+    void Joueur::ShowJetons(){//todo
+		for (int i = 0; i < 10; i++)
+		{
+			
+		}
+	}
+
+	void Joueur::ShowCartes() {
+		for (vector<Carte>::iterator i = Cartes.begin(); i != Cartes.end(); i++)
+			cout << *i << endl; 
+	};
+
+	void Joueur::ShowReserved() {
+		for (vector<Carte>::iterator i = Reserved.begin(); i != Reserved.end(); i++)
+			cout << *i << endl;
+	};
+
+	Couleur Joueur::GetBonus() { //todo
+		}
 
 
     Controleur::Controleur() {
