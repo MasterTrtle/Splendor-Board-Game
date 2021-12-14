2d0d0dbedd8d918a88e2fd312e789f595d252dd8
Nicolas Vengeon <nicolas.vengeon@etu.utc.fr>
2021-12-14 22:52:31 +0000

    Upload New File

---
+++ ostream.cpp
@@ -0,0 +1,28 @@
+
+
+#include "splendor.h"
+#include <string>
+#include <iostream>
+using namespace std;
+using namespace materiel;
+using namespace Splendor;
+
+
+std::ostream& operator<<(std::ostream& f, TypeCarte t) {
+    if (t == TypeCarte::N1) f << "N1";
+    else if (t == TypeCarte::N2) f << "N2";
+    else if (t == TypeCarte::N3) f << "N3";
+    else throw SplendorException("Il faut ajouter les autres types de carte");
+    return f;
+}
+
+std::ostream& operator<<(std::ostream& f, Prix c) {
+    f << "Vert:" << c.vert << "Bleu:" << c.bleu << "Noir:" << c.noir << "Rouge:" << c.rouge << "Blanc:" << c.blanc;
+    return f;
+}
+
+std::ostream& operator<<(std::ostream& f, const materiel::Carte& c) {
+    f << "(" << "ID:" << c.getID() << "," << "Nom:" << c.getNom() << "," << "Prix:" << c.getPrix() << ","
+        << "ID:Perstige" << c.getPrestige() << ")";
+    return f;
+}
